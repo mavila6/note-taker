9d80d9b76a1638f18f5278d26aa53f90a5375a95
Marco Avila <mavila6@email.arizona.com>
2021-10-01 04:38:17 -0700

    added router.get function to the api routes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 const fs = require("fs");
 
-router.get("", (req, res) => {
-    
-})
+router.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "UTF8", (err, data) => {
+        const notes = JSON.parse(data);
+        res.send(notes);
+        // console.log(data);
+        if (err) {
+            throw err;
+        };
+    });
+});
 
-router.post("", (req, res) => {
+// router.post("", (req, res) => {
 
-})
+// });
 
-router.delete("", (req, res) => {
+// router.delete("", (req, res) => {
 
-})
+// });
+
+module.exports = router;
