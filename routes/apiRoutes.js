4e4795222321e03af13addaafcf0b17b8d98c986
Marco Avila <mavila6@email.arizona.com>
2021-10-01 04:19:57 -0700

    added to the api routes file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const fs = require("fs");
+
+router.get("", (req, res) => {
+    
+})
+
+router.post("", (req, res) => {
+
+})
+
+router.delete("", (req, res) => {
+
+})
